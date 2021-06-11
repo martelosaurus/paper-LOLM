0c9a55f998d297a177cdfc9e445d83b3758a7a6a
jordan <jordan@jordanmartel.com>
2021-06-11 05:38:52 -0400

    ENH:

--- DataPlots/venture_capital_dats/venture_capital_build.r
+++ DataPlots/venture_capital_dats/venture_capital_build.r
@@ -1,12 +1,8 @@
 # ------------------------------------------------------------------------------
 library(data.table)
-library(ggplot2)
-library(mgcv)
-library(plyr)
-library(lfe)
 library(zoo)
 
-dummy=F
+dummy=TRUE
 
 # load data
 if (is.element("venture_capital.RData",list.files())) {
@@ -28,18 +24,14 @@ if (is.element("venture_capital.RData",list.files())) {
 
 	# drop missing company id
 	X=X[company.id != "-"]
+	X=X[!is.na(amount)&amount>0]
 
 	# column types
 	X[, amount := as.numeric(amount)]
 	X[, valuation := as.numeric(valuation)]
 	X[, equity.invested := as.numeric(equity.invested)]
 
-	# drop missing round investment amount
-	X=X[!is.na(amount)]
-	X=X[amount>0]
-
 	# calculate total investment per round reported at fund level 
-	# (used for comparison with fund round amount)
 	X[,equity:=sum(equity.invested,na.rm=T),by=.(company.id, round.number)]
 	
     # --------------------------------------------------------------------------
@@ -61,15 +53,18 @@ if (is.element("venture_capital.RData",list.files())) {
 	X[,nleads:=as.integer(sum(lead.vc)),by=.(company.id,round.number)]
 	X[,lead.firm:=ifelse(lead.vc==T,firm.name,NA)]
 
-	# COMPUTE MAXIMUM NUMBER OF LEADS
+	# maximum number of leads (and labels)
 	L = max(X[,nleads])
-	#X[,paste("lead.firm",1:max_leads):=,by=.(company.id,round.number)]
+	L = 3
+	lead_firms = paste("lead.firm",1:L,sep=".")
+	#X[,c(lead_firm):=.SD[lead.firm,],by=.(company.id,round.number)]
 
 	X[nleads >= 1L, lead.firm.1 := na.exclude(unique(lead.firm))[1], by=.(company.id, round.number)]
 	X[nleads >= 2L, lead.firm.2 := na.exclude(unique(lead.firm))[2], by=.(company.id, round.number)]
 	X[nleads >= 3L, lead.firm.3 := na.exclude(unique(lead.firm))[3], by=.(company.id, round.number)]
 
 	X[, nleads := NULL]
+	if (TRUE) {
 
 	setkey(X, company.id, investment.date)
 	
@@ -83,7 +78,6 @@ if (is.element("venture_capital.RData",list.files())) {
 
 	### Before this: data is one record per investment firm round
 	### After this: data is one record per company.id/round
-	# collapse to to company.id/round
 	X=unique(X)
 
 	# add cumulative amount and equity (for filtering purposes)
@@ -91,7 +85,6 @@ if (is.element("venture_capital.RData",list.files())) {
 	X[,cum.amount:=cumsum(amount),by=.(company.id)]
 
 	# drop rounds with no reported investment up to that point 
-	# (helps to eliminate NA lead investor later on)
 	X=X[cum.equity>0]
 
 	# figure out if previous round is in (otherwise data is useless)
@@ -101,7 +94,6 @@ if (is.element("venture_capital.RData",list.files())) {
 	X[,last.in:=ifelse(last.round==(round.number-1),1L,0L)]
 
 	# check that valuation and amount reported for all rounds
-	X[,valuation.adj:=valuation-amount]
 	X[,round.Return:=(valuation-amount)/shift(valuation),by=company.id]
 	X[,round.Return:=ifelse(is.na(round.Return),1,round.Return)]
 	X[,cum.Return:=cumprod(round.Return),by=company.id]
@@ -110,12 +102,11 @@ if (is.element("venture_capital.RData",list.files())) {
 	# fill-in missing lead firms
 	setkey(X, company.id, round.number)
 
-	# if lead.firm.1 is NA, then last leads are retained 
+	# if lead.firm.1 is NA, then the current lead is the last lead
 	X[,lead.firm.1:=na.locf(lead.firm.1),by=company.id]
 
 	# lag some values to figure out lead changes
 	L=3
-	lead_firms = paste("lead.firm",1:L,sep=".")
 	last_leads = paste("last.lead",1:L,sep=".")
 	X[,c(last_leads):=shift(.SD[,lead_firms,with=F]),by=company.id]
 
@@ -124,13 +115,9 @@ if (is.element("venture_capital.RData",list.files())) {
 
 	# if all of the leads are new, then a lead change has occurred
 	setkey(X, company.id, round.number)
-	X[, uid := 1:.N]
-	X[, lead.change := ifelse( any(na.exclude(c(lead.firm.1, lead.firm.2, lead.firm.3)) %in% na.exclude(c(last.lead.1, last.lead.2, last.lead.3))), 0L, 1L), by=uid]
+	X[, lead.change := !any(na.exclude(c(lead.firm.1, lead.firm.2, lead.firm.3)) %in% na.exclude(c(last.lead.1, last.lead.2, last.lead.3))), by=index(X)]
 
-	# if the lead firm is NA, there was no lead change (but this shouldn't happen?)
-	X[is.na(lead.firm.1),lead.change:=F]
-
-	# check that all last rounds are included (last.in.sum == round.number, cut is further down)
+	# check that all last rounds are included 
 	X[,last.in.sum:=cumsum(last.in),by=company.id]
 	X=X[round.number==last.in.sum]
 
@@ -148,17 +135,15 @@ if (is.element("venture_capital.RData",list.files())) {
 	# more filters
 
 	# X%+ of all investments must be accounted for 
-	# (should be up until change)
 	X[,fraction.reported:=cum.equity/cum.amount]
-	X=X[fraction.reported >= .8]
+	X=X[fraction.reported>=.8]
 
 	# Lead change to undisclosed firm - unclear whether to exclude or allow 
 	# (could add noise - but also informative)
 	X=X[lead.firm.1!="Undisclosed Firm"]
 	X=X[last.lead.1!="Undisclosed Firm"]
 
-	# Now create plots following same procedure as with machine data
-	X=X[!is.na(valuation.adj)]
+	# more filters
 	X=X[!is.na(last.valuation)]
 	X=X[last.cum.Return>0] 
 
@@ -172,8 +157,15 @@ if (is.element("venture_capital.RData",list.files())) {
 
 	# --------------------------------------------------------------------------
 	# drop
+
+	# check against old version
 	load("control.RData")
+	X[,lead.change:=1L*lead.change]
+	X[,valuation.adj:=valuation-amount]
+	X=X[!is.na(valuation.adj)]
 	if (all(X %in% dt_vca)) {print("SUCCESS")}
+
+	# factor and drop
 	X=X[,.(last.investment.date,investment.date,duration,logret)]
 	factor_yq=function(x.date) factor(year(x.date)):factor(quarter(x.date))
 	X[,t.buy.yq:=factor_yq(last.investment.date)]
@@ -181,4 +173,4 @@ if (is.element("venture_capital.RData",list.files())) {
 	X[,c("last.investment.date","investment.date"):=NULL]
 	#save(X,file="venture_capital.RData")
 	
-}
+}}
